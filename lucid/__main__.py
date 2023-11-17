fc7c6b67414f4c9a1ac74867c60656221e3b5e95
= <nmaxwell@d2.com>
2023-11-17 02:32:07 -0800

    update __main__.py for code compiling

--- lucid/__main__.py
+++ lucid/__main__.py
@@ -1,3 +1,13 @@
-from lucid.cli import main
+import subprocess
+from pathlib import Path
 
-main()
+
+def main() -> None:
+    launcher_path = Path(Path(__file__).parent, 'launcher')
+    launcher_file = Path(launcher_path, 'lucid_launcher.bat')
+    p = subprocess.Popen(launcher_file.as_posix(), cwd=launcher_path.as_posix())
+    p.communicate()
+
+
+if __name__ == '__main__':
+    main()
