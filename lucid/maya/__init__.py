3878801948f7b22058c2b3d72e25fc732a018a23
nate-maxwell <natedmaxwell@gmail.com>
2023-11-17 18:42:44 -0800

    added maya undo chunk

--- lucid/maya/__init__.py
+++ lucid/maya/__init__.py
@@ -1,5 +1,16 @@
-"""Lucid Maya"""
+"""
+# Lucid Maya
 
+* Description
+
+    Lucid's primary maya module.
+
+* Update History
+
+    `2023-09-22` - Init
+
+    `2023-11-17` - Added UndoChunk.
+"""
 
 from typing import Callable
 
@@ -12,6 +23,30 @@ from maya import cmds
 def get_maya_window() -> QtWidgets.QMainWindow:
     """Return the Maya main window widget as a python object."""
     return shiboken2.wrapInstance(int(OpenMayaUI.MQtUtil.mainWindow()), QtWidgets.QMainWindow)
+
+
+class UndoChunk(object):
+    """
+    Lucid manager for a Maya undoChunk.
+
+    Everything under the `with` statement will be grouped into a single undo
+    action. This is similar to pymel's `pymel.core.system.UndoChunk`, with the
+    exception that this implementation forces you to provide an undo name.
+
+    Examples:
+        >>> with UndoChunk('Duplicate and mirror'):
+        >>>     # Everything here will be part of a single undoChunk
+        >>>     ...
+        >>>
+    """
+    def __init__(self, chunk_name: str):
+        self.chunk_name = chunk_name
+
+    def __enter__(self):
+        cmds.undoInfo(openChunk=True, chunkName=self.chunk_name)
+
+    def __exit__(self, exc_type, exc_val, traceback):
+        cmds.undoInfo(closeChunk=True)
 
 
 def retain_selection(func: Callable):
