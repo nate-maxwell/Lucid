637cae4b0e1ae036d5c65eb89773158e0a663d4f
= <nmaxwell@d2.com>
2023-12-21 00:16:58 -0800

    expanded debug functions

--- lucid/debug.py
+++ lucid/debug.py
@@ -21,6 +21,10 @@ import lucid.io_utils
 import lucid.constants
 
 
+class DebugException(lucid.LucidException):
+    """An exception class for debug related errors."""
+
+
 def print_environ_vars(prefix: str = '') -> None:
     """
     Prints the current os environment vars.
@@ -87,3 +91,44 @@ def print_name_and_exec_time(func: Callable):
         func(*args, **kwargs)
 
     return wrapper
+
+
+def debug_by_dev_level(dev_level: int):
+    """
+    A debug decorator tied to the ENV_DEV_LEVEL environment var.
+    If debug_level <= ENV_DEV_LEVEL's value, then the debug contents
+    will execute, otherwise the func will execute as normal.
+
+    Args:
+        dev_level(int): The required development level before
+        the decorated function gets wrapped.
+    """
+    def evaluate_dev_level(func: Callable):
+        """
+        Currently the debug print will print the function's name
+        and the time it took to execute.
+        """
+        if not 0 < dev_level < 5:
+            raise DebugException(f'Is your debug decorator level not between 1 and 5 for {func.__name__}?')
+
+        env_dev_level = os.environ.get(lucid.constants.ENV_DEV_LEVEL, '1')
+        if dev_level <= int(env_dev_level):
+            @timer
+            def wrapper(*args, **kwargs):
+                print(f'Function:: {func.__name__}')
+                print('\nargs=')
+                print(args)
+                print('\nkwargs=')
+                for k, v in kwargs.items():
+                    print(f'{k}: {v}')
+                print('\n')
+                func(*args, **kwargs)
+                print('\n')
+
+        else:
+            def wrapper(*args, **kwargs):
+                func(*args, **kwargs)
+
+        return wrapper
+
+    return evaluate_dev_level
