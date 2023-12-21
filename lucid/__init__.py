0d663a09067c784488addfb5b72ad5886368a380
= <nmaxwell@d2.com>
2023-12-21 00:16:47 -0800

    project base exception class

--- lucid/__init__.py
+++ lucid/__init__.py
@@ -4,3 +4,14 @@ Lucid Unreal Games Pipeline.
 
 
 __version__ = '1.0.0'
+
+
+class LucidException(Exception):
+    """Base Lucid exception. All other exception should be derived from this class."""
+    def __init__(self, *args):
+        if len(args) == 0:
+            super().__init__()
+        elif len(args) == 1:
+            super().__init__('[Lucid] ' + str(args[0]))
+        else:
+            super().__init__('[Lucid]', *args)
