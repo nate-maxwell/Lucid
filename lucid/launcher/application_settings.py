e02dba05fe67edb86e020fb8941d7f6f52339aff
nate-maxwell <natedmaxwell@gmail.com>
2025-07-24 23:39:46 -0700

    simplified application selector widget methods

--- lucid/launcher/application_settings.py
+++ lucid/launcher/application_settings.py
@@ -18,13 +18,7 @@ class ApplicationSettings(LGroupBox):
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.name = name
-        self.create_widgets()
-        self.create_layout()
-
-    def create_widgets(self) -> None:
         self.file_selector = FileSelector('EXE Path:')
-
-    def create_layout(self) -> None:
         self.add_widget(self.file_selector)
 
     @property
